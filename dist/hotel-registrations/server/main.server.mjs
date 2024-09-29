d8b8c0ccf108112a49e0046997add09abe236224
aline <aline.silva@joinin.com.br>
2024-09-29 14:18:30 -0300

    Adjust index.html

---
+++ dist/hotel-registrations/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SR32O66W.mjs";import"./chunk-JPIZIY7O.mjs";import"./chunk-2DZKNPVD.mjs";import"./chunk-Q4EYKFCA.mjs";import"./chunk-AP7AGP6A.mjs";import"./chunk-Q34TFRY2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
